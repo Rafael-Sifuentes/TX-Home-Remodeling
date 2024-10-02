fa591c8581b8a02520673d8923f6a5b24211898e
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2024-10-02 12:02:52 -0500

    commit__2
    New website layout and gallery page addition

---
+++ assets/Home-page/home.js
@@ -0,0 +1,110 @@
+
+
+const navMenu = document.getElementById("nav-list")
+const toggleNavBtn = document.getElementById("nav-toggle-btn")
+
+if(toggleNavBtn){
+  toggleNavBtn.addEventListener('click', () =>{
+    navMenu.classList.toggle('show-nav')
+    toggleNavBtn.classList.toggle('rotate')
+  })
+}
+
+const navlink = document.querySelectorAll('.nav-list-item')
+
+const linkAction = () =>{
+const navMenu = document.getElementById('nav-list')
+    navMenu.classList.remove('show-nav')
+    toggleNavBtn.classList.toggle('rotate')
+
+}
+navlink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+const blurHeader = () => {
+    const header = document.getElementById('header')
+  
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        :header.classList.remove('blur-header')
+  }
+  
+  window.addEventListener('scroll' , blurHeader)
+
+
+/*=============== SCROLL SECTIONS ACTIVE ===============*/
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav-list a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link');
+		}else{
+			sectionsClass.classList.remove('active-link');
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+
+var swiper = new Swiper('.projects-swiper-container', {
+	slidesPerView: 1,
+	spaceBetween: 32,
+	speed: 800,
+	loop: true,
+  
+	autoplay: {
+		delay: 2000,
+		disableOnInteraction: true,
+	  },
+  
+	  navigation: {
+		nextEl: '.project-next',
+		prevEl: '.project-prev',
+	},
+
+	breakpoints: {
+		600: {
+			slidesPerView: 2,
+			spaceBetween: 16,
+		}
+	}
+
+	
+  });
+
+  var reviewSwiper = new Swiper('.reviews-swiper-container', {
+	slidesPerView: 1,
+	spaceBetween: 32,
+	speed: 800,
+	loop: true,
+  
+	autoplay: {
+		delay: 2000,
+		disableOnInteraction: true,
+	  },
+  
+	  navigation: {
+		nextEl: '.review-next',
+		prevEl: '.review-prev',
+	},
+	
+	//   breakpoints: {
+	//     600: {
+	//         slidesPerView: 2,
+	//     },
+  
+	//     1000: {
+	//       slidesPerView: 3,
+	//     },
+	// },
+  });
